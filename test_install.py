b9c6ff37b16fb46622e5741992c97df8a7c92e02
nameisahmedh <mdqamarahmed123@gmail.com>
2025-05-10 15:44:37 +0530

    Initial commit

---
+++ test_install.py
@@ -0,0 +1,14 @@
+import numpy
+import pandas
+import matplotlib
+import sklearn
+import shap
+import lightgbm
+import xgboost
+import catboost
+import imblearn
+import keras
+import tensorflow
+import h5py
+import google.protobuf
+print("✅ All packages imported successfully!")
